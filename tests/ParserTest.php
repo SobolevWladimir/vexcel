a1b72528ceac0165b2ebc8922b515e5372109734
Владимир? <Sobolev.Wladim@yandex.ru>
2023-07-24 14:43:51 +0300

    добавил пару вариантов для тестирования

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -6,6 +6,10 @@ use PHPUnit\Framework\TestCase;
 
 final class ParserTest extends TestCase
 {
+    /* x+y*z  = “x+(y*z)”*/
+  // “x+(y*z)”
+  // “a+b+(c+d)*e*f+g”.
+
     // public function testParse()
     // {
     //     $this->assertSame('2', '3');
