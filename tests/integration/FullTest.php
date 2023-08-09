dfe338158b215cbc3def842a886ab20278d3e06d
Владимир? <Sobolev.Wladim@yandex.ru>
2023-08-09 18:06:44 +0300

    Добавил тестеов

--- tests/integration/FullTest.php
+++ tests/integration/FullTest.php
@@ -3,14 +3,39 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Wladimir\ParserExcel\Parser\Parser;
 
 final class FullTest extends TestCase
 {
-    public function testInt(): void
+    /**
+     * @return array<int,mixed>
+     */
+    public static function additionProvider(): array
+    {
+        return [
+        ['3', 3],
+        ['3+3', 6 ],
+        ['"test"', "test"],
+        ['3+"test"', "3test"],
+        ['3*(2+2)', 12],
+        ['3*2+2', 8],
+        ['ЕСЛИ(2=2;"ДА";"НЕТ")', "ДА"],
+        ['ЕСЛИ(2=2;3+5;3-2)', 8],
+        ['ЕСЛИ(2=3;"ДА";"НЕТ")', "НЕТ"],
+        ['ЕСЛИ(2=3;3+5;3-2)', 1],
+        ['ЕСЛИ(2>1;"ДА";"НЕТ")', "ДА"],
+        ['ЕСЛИ(2>3;"ДА";"НЕТ")', "НЕТ"],
+        ['ЕСЛИ(1<2;"ДА";"НЕТ")', "ДА"],
+        ['ЕСЛИ(3<2;"ДА";"НЕТ")', "НЕТ"],
+        ];
+    }
+
+    #[DataProvider('additionProvider')]
+    public function testProvider(string $code, mixed $expected): void
     {
         $sut = new Parser();
-        $formula = $sut->parse('3');
-        self::assertSame($formula->calculate(), 3);
+        $formula = $sut->parse($code);
+        $this->assertEquals($expected, $formula->calculate());
     }
 }
