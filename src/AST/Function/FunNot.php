c06ef5e5374e66c7b0439c5fbf50a04cb755a356
Владимир? <Sobolev.Wladim@yandex.ru>
2023-08-13 09:09:16 +0300

    Написал тест на переменные

---
+++ src/AST/Function/FunNot.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Wladimir\ParserExcel\AST\Function;
+
+use Wladimir\ParserExcel\Repository\ValueRepositoryInterface;
+
+class FunNot extends AbstractFunction
+{
+    protected function getNumberArguments(): int
+    {
+        return 1;
+    }
+
+    public function calculate(?ValueRepositoryInterface $repository = null): mixed
+    {
+        $value = $this->args[0]->calculate($repository);
+        if ($value) {
+            return false;
+        }
+        return true;
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return [];
+    }
+}
--- tests/integration/FullTest.php
+++ tests/integration/FullTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
+use Tests\Data\ValueRepositoryFake;
 use Wladimir\ParserExcel\Parser\Parser;
 
 final class FullTest extends TestCase
@@ -11,14 +12,18 @@ final class FullTest extends TestCase
     /**
      * @return array<int,mixed>
      */
-    public static function additionProvider(): array
+    public static function notVariableProvider(): array
     {
         return [
             ['3', 3],
             ['3+3', 6],
+            ['3-3', 0],
+            ['3/3', 1],
+            ['3*3', 9],
             ['"test"', 'test'],
             ['3+"test"', '3test'],
             ['3*(2+2)', 12],
+            ['3*(2+2+(1*1))', 15],
             ['2+2*3', 8],
             ['ЕСЛИ(2=2;"ДА";"НЕТ")', 'ДА'],
             ['ЕСЛИ(2=2;3+5;3-2)', 8],
@@ -28,14 +33,53 @@ final class FullTest extends TestCase
             ['ЕСЛИ(2>3;"ДА";"НЕТ")', 'НЕТ'],
             ['ЕСЛИ(1<2;"ДА";"НЕТ")', 'ДА'],
             ['ЕСЛИ(3<2;"ДА";"НЕТ")', 'НЕТ'],
+            ['ЕСЛИ(НЕ(3<2);"ДА";"НЕТ")', 'ДА'],
+            ['ЕСЛИ(НЕ(3>2);"ДА";"НЕТ")', 'НЕТ'],
         ];
     }
 
-    #[DataProvider('additionProvider')]
-    public function testProvider(string $code, mixed $expected): void
+    /**
+     * @return array<int,mixed>
+     */
+    public static function variableProvider(): array
+    {
+        return [
+            ['ТРИ', 3],
+            ['ТРИ+3', 6],
+            ['ТРИ-3', 0],
+            ['ТРИ/3', 1],
+            ['ТРИ*3', 9],
+            ['ТРИ+"test"', '3test'],
+            ['ТРИ*(ДВА+2)', 12],
+            ['ТРИ*(ДВА+2+(1*1))', 15],
+            ['ДВА+2*ТРИ', 8],
+            ['ЕСЛИ(ДВА=2;"ДА";"НЕТ")', 'ДА'],
+            ['ЕСЛИ(ДВА=2;3+5;3-2)', 8],
+            ['ЕСЛИ(ДВА=ТРИ;"ДА";"НЕТ")', 'НЕТ'],
+            ['ЕСЛИ(ДВА=3;3+5;3-2)', 1],
+            ['ЕСЛИ(ДВА>ОДИН;"ДА";"НЕТ")', 'ДА'],
+            ['ЕСЛИ(ДВА>3;"ДА";"НЕТ")', 'НЕТ'],
+            ['ЕСЛИ(ОДИН<ДВА;"ДА";"НЕТ")', 'ДА'],
+            ['ЕСЛИ(ТРИ<ДВА;"ДА";"НЕТ")', 'НЕТ'],
+            ['ЕСЛИ(НЕ(ТРИ<ДВА);"ДА";"НЕТ")', 'ДА'],
+            ['ЕСЛИ(НЕ(ТРИ>ДВА);"ДА";"НЕТ")', 'НЕТ'],
+        ];
+    }
+
+    #[DataProvider('notVariableProvider')]
+    public function testNotVaiableProvider(string $code, mixed $expected): void
     {
         $sut = new Parser();
         $formula = $sut->parse($code);
         self::assertSame($expected, $formula->calculate());
+    }
+
+    #[DataProvider('variableProvider')]
+    public function testVariableProvider(string $code, mixed $expected): void
+    {
+        $sut = new Parser();
+        $formula = $sut->parse($code);
+        $repository  = new ValueRepositoryFake();
+        self::assertSame($expected, $formula->calculate($repository));
     }
 }
